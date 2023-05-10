3553c1996dba1de5859c4393c1094cac41940ff4
yyb <1033750222@qq.com>
2023-05-10 22:31:48 +0800

    warichu component & demo page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,124 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FC, useId, useState } from "react";
+import "./main.css";
+import { Warichu } from "./Warichu";
+
+type BlockType = "TEXT" | "WARICHU";
+type TextBlock = {
+  type: BlockType;
+  content: string;
+};
+
+const initBlocks: TextBlock[] = [
+  {
+    type: "TEXT",
+    content:
+      "『漢字御廃止之議』は、将軍に上申後久しく世に知られなかったが、明治32年に前島と同郷後進のかな文字論者小西信八が、国字改良論の最先覚としての前島の功を顕彰しようとして、『漢字御廃止之議』の草案をはじめ、『国文教育之儀ニ付建議』『国文教育施行ノ方法』",
+  },
+  {
+    type: "WARICHU",
+    content: "共に明治2年集議院に提出",
+  },
+  {
+    type: "TEXT",
+    content: "『学制御施行ニ先タチ国字改良相成度卑見内申書』",
+  },
+  {
+    type: "WARICHU",
+    content: "明治六年右大臣岩倉具視に上申",
+  },
+  {
+    type: "TEXT",
+    content: "『興国文廃漢字議』",
+  },
+  {
+    type: "WARICHU",
+    content: "同六年政府にも建議せんとして起草、都合により不提出",
+  },
+  {
+    type: "TEXT",
+    content:
+      "の後の建白文をも合編、それに『前島密君国字国文改良建議書』の表題を付けて印刷し非売配布した小冊子によって、初めて一部の人々に知られ、更に33年4月国字改良の世論に応えて文部省が8名の国語調査委員を創設した際、その委員長を委嘱された前島が、「太陽」記者のもとめで同誌5月号に寄せられた『国語調査の意見』中に掲出した、同建白書の枢要な部分の公表によって、更に一般の知るところとなった",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [textBlocks, setTextBlocks] = useState<TextBlock[]>(initBlocks);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="input-area">
+        {textBlocks.map((tb) => (
+          <BlockDisplay
+            key={tb.content}
+            textBlock={tb}
+            deleteTextBlock={() =>
+              setTextBlocks((prev) => prev.filter((item) => item !== tb))
+            }
+          />
+        ))}
+        <BlockInput
+          addTextBlock={(tb) => setTextBlocks((prev) => [...prev, tb])}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="text-area">
+        {textBlocks.map((tb) =>
+          tb.type === "TEXT" ? (
+            <span key={tb.content}>{tb.content}</span>
+          ) : (
+            <Warichu key={tb.content} content={tb.content} />
+          )
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+const BlockDisplay: FC<{
+  textBlock: TextBlock;
+  deleteTextBlock: () => void;
+}> = ({ textBlock, deleteTextBlock }) => {
+  return (
+    <div className="block-display">
+      <label>割注</label>
+      <input type="checkbox" checked={textBlock.type === "WARICHU"} disabled />
+      <input type="text" value={textBlock.content} disabled />
+      <button onClick={deleteTextBlock}>削除</button>
+    </div>
+  );
+};
+
+const BlockInput: FC<{ addTextBlock: (tb: TextBlock) => void }> = ({
+  addTextBlock,
+}) => {
+  const [content, setContent] = useState("");
+  const [type, setType] = useState<BlockType>("TEXT");
+  const checkboxId = useId();
+
+  return (
+    <div className="block-input">
+      <label htmlFor={checkboxId}>割注</label>
+      <input
+        id={checkboxId}
+        type="checkbox"
+        checked={type === "WARICHU"}
+        onChange={(e) => setType(e.target.checked ? "WARICHU" : "TEXT")}
+      />
+      <input
+        type="text"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          if (!content) return;
+          addTextBlock({ type, content });
+          setContent("");
+        }}
+      >
+        追加
+      </button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Warichu.tsx
@@ -0,0 +1,109 @@
+import {
+  CSSProperties,
+  FC,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+
+export const Warichu: FC<{ content: string }> = ({ content }) => {
+  const halfContent = useMemo(() => {
+    const midIndex = Math.ceil(content.length / 2);
+    const str1 = content.slice(0, midIndex);
+    const str2 = content.slice(midIndex);
+    const span1 = document.createElement("span");
+    const span2 = document.createElement("span");
+    span1.innerText = str1;
+    span2.innerText = str2;
+    document.body.append(span1);
+    const { width: w1 } = span1.getBoundingClientRect();
+    span1.remove();
+    document.body.append(span2);
+    const { width: w2 } = span2.getBoundingClientRect();
+    span2.remove();
+    return w1 > w2 ? str1 : str2;
+  }, [content]);
+  const phStartEl = useRef<HTMLElement>(null);
+  const phEndEl = useRef<HTMLElement>(null);
+  const wrapperEl = useRef<HTMLElement>(null);
+
+  const [beginTop, setBeginTop] = useState(0);
+  const [endBottom, setEndBottom] = useState(0);
+  const [lineHeight, setLineHeight] = useState(0);
+  const [fontSize, setFontSize] = useState(0);
+  const lineMargin = lineHeight / 2 - (2 * fontSize) / 3;
+  const [rows, setRows] = useState(0);
+
+  const polygon = useMemo(() => {
+    let offset = 0;
+    const tempPath = [];
+    for (let i = 0; i < rows - 1; i++) {
+      tempPath.push(
+        `${offset}px 0`,
+        `${offset}px 100%`,
+        `${offset + lineMargin * 2}px 100%`,
+        `${offset + lineMargin * 2}px 0`
+      );
+      offset += lineHeight;
+    }
+    if (rows > 1) offset -= lineHeight;
+    tempPath.push(
+      `${offset}px 0`,
+      `${offset}px ${beginTop}px`,
+      `100% ${beginTop}px`,
+      `100% 0`
+    );
+    return "polygon(" + tempPath.join(", ") + ")";
+  }, [lineHeight, lineMargin, rows, beginTop]);
+
+  useLayoutEffect(() => {
+    const resetStyle = () => {
+      if (!phStartEl.current || !phEndEl.current || !wrapperEl.current) return;
+      const { top: phStartTop, left: phStartLeft } =
+        phStartEl.current.getBoundingClientRect();
+      const { top: wrapperTop, bottom: wrapperBottom } =
+        wrapperEl.current.getBoundingClientRect();
+      const { bottom: phEndBottom, left: phEndLeft } =
+        phEndEl.current.getBoundingClientRect();
+      setBeginTop(phStartTop - wrapperTop);
+      setEndBottom(wrapperBottom - phEndBottom);
+
+      const computedStyle = window.getComputedStyle(phStartEl.current);
+
+      const lineHeight = parseFloat(computedStyle.lineHeight);
+      const fontSize = parseFloat(computedStyle.fontSize);
+      const rows = Math.round((phStartLeft - phEndLeft) / lineHeight + 1);
+      setRows(rows);
+      setFontSize(fontSize);
+      setLineHeight(lineHeight);
+    };
+
+    window.addEventListener("resize", resetStyle);
+    resetStyle();
+    return () => window.removeEventListener("resize", resetStyle);
+  }, []);
+
+  return (
+    <span className="warichu">
+      <span
+        className="warichu-wrapper"
+        ref={wrapperEl}
+        style={
+          {
+            "--polygon": polygon,
+            "--big-line-height": lineHeight + "px",
+            "--end-bottom": endBottom + "px",
+            "--before-height": rows === 1 ? beginTop + "px" : "100%",
+            width: rows * lineHeight + "px",
+          } as CSSProperties
+        }
+      >
+        <span className="warichu-text">{content}</span>
+      </span>
+      <i ref={phStartEl}></i>
+      <span className="warichu-placeholder">{halfContent}</span>
+      <i ref={phEndEl}></i>
+    </span>
+  );
+};
